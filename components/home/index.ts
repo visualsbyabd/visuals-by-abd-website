4cdf6ff965043b083fe17c093c016aa40b004a7a
visualsbyabd <visualsbyabd257@gmail.com>
2024-11-29 16:41:41 +0200

    Initial Commit: Home Components Package Init

---
+++ components/home/index.ts
@@ -0,0 +1,11 @@
+import HeroSection from "./HeroSection";
+import AboutMeSection from "./AboutMeSection";
+import ServicesSection from "./ServicesSection";
+import BestSellingSection from "./BestSellingSection";
+import LatestProjectsSection from "./LatestProjectsSection";
+
+export { HeroSection as HeroSection }
+export { AboutMeSection as AboutMeSection }
+export { ServicesSection as ServicesSection }
+export { BestSellingSection as BestSellingSection }
+export { LatestProjectsSection as LatestProjectsSection }
